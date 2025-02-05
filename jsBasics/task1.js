a6a0f6162aa94849db59bdcae044772cc520230b
alla <alla@ravin-ai.com>
2025-02-05 12:51:08 +0200

    Add tasks for lesson 4 JavaScript primitive data types

---
+++ jsBasics/task1.js
@@ -0,0 +1,14 @@
+const string = 'Studying is hard';  //New string variable initialized 
+console.log(string); 
+
+const number = 100; //New number variable initialized 
+console.log(number); 
+
+const bool = true; //New boolean variable initialized 
+console.log(bool);
+
+const someNullVar = null; //New null variable initialized 
+console.log(someNullVar); 
+
+let someUndefVar; //New undefined variable declared 
+console.log(someUndefVar);
---
+++ jsBasics/task4.3.js
@@ -0,0 +1,7 @@
+let radius = 10;
+
+let height = 20;
+
+let volume = Math.PI * radius * radius * height; // Calculate the cylinder volume
+
+console.log(volume);
